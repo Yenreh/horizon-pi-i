6ae7e6ba1c91983946109317f6d349911d8bcd32
yenreh <qherney@gmail.com>
2025-04-23 06:39:45 -0500

    Add navigation links to Header component

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,16 +1,16 @@
-import { NavLink } from "react-router";
+import {NavLink} from "react-router";
 import "./Header.css";
 
 const Header = () => {
-  return (
-    <header>
-      <nav>
-        <NavLink to="/" end>
-          Inicio
-        </NavLink>
-      </nav>
-    </header>
-  );
+    return (<header>
+            <nav>
+                <NavLink to="/" end>Inicio</NavLink>
+                <NavLink to="/cataratas">Cataratas</NavLink>
+                <NavLink to="/miopia">Miopía</NavLink>
+                <NavLink to="/enfermedar3">Enfermedad 3</NavLink>
+                <NavLink to="/enfermedar3">Enfermedad 4</NavLink>
+            </nav>
+        </header>);
 };
 
 export default Header;
