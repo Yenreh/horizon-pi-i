3814fbf9fa1be82cf396671340a07319e5435f71
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-04-22 19:59:02 -0500

    Cambios en la definición para adecuarse a la plantilla

--- src/pages/diseases/retina-detachment/RdDefinition.jsx
+++ src/pages/diseases/retina-detachment/RdDefinition.jsx
@@ -1,16 +1,16 @@
-
+import "./RdDefinition.css"
 
 const RdDefinition = () => {
     return (
         <div className="container">
-            <div>
+            <div className="inferior-curve">
                 <img src="/images/retina-detachment-presentation.jpg" alt="Imagen decorativa para incio de la explicacion" className="main-image" />
-                <div>DESPRENDIMIENTO DE RETINA</div>
+                <div className="title-overlay-inferior-left"> DESPRENDIMIENTO DE RETINA</div>
             </div>
 
             <div className="content">
                 <div className="info-section">
-                    <div className="section-title">¡Que es?</div>
+                    <div className="section-title">¿Que es?</div>
                     <p>
                     La retina es un tejido neural multilaminar que recubre la parte posterior del ojo y actúa como una extensión del sistema nervioso central, encargada de convertir la luz en señales eléctricas que viajan al cerebro a través del nervio óptico. Está compuesta por 11 tipos celulares organizados en capas nucleares y sinápticas. Las cuales permiten el procesamiento paralelo de la información visual. Su estructura anatómica incluye desde la membrana limitante interna hasta el epitelio pigmentario, lo que le permite desempeñar funciones tanto sensoriales como neurológicas.
                     </p>
