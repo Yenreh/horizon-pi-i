199cd76c7295ba18ce4a918e031532659e8b9037
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-04-27 10:23:33 -0500

    Pagina de NotFound actualizada

--- src/pages/diseases/retina-detachment/tabs/RdPrevention.jsx
+++ src/pages/diseases/retina-detachment/tabs/RdPrevention.jsx
@@ -8,13 +8,13 @@ export default function RdPrevention() {
         <Row className="align-items-center gy-4">
           <Col md={6}>
             <div className="desease-canvas-wrapper">
-                <h1>Tratamientos</h1>
+                <h1>Prevención</h1>
             </div>
           </Col>
           <Col md={6}>
             <div className="desease-text-definition p-4">
               <h2 className="mb-3" style={{ color: "var(--color-2)" }}>
-                Tratamientos
+              Prevención
               </h2>
               <p>Aquí finalmente sabras como prevenirla</p>
             </div>
