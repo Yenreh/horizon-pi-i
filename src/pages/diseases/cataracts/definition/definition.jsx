5fea6af5e8ee47bc12436c7d3c58b8e29cee2e96
SheilaV7 <sheila.valencia@correounivalle.edu.co>
2025-04-26 21:27:25 -0500

    Actualización cataratas

--- src/pages/diseases/cataracts/definition/definition.jsx
+++ src/pages/diseases/cataracts/definition/definition.jsx
@@ -8,28 +8,6 @@ import Lights from "./Lights/Lights";
 export default function definition() {
   return (
       <div className="content">
-        <div className="info-section">
-          <div className="section-title">¿Qué son?</div>
-          <p>
-          Las cataratas oculares son una condición visual común que ocurre cuando el cristalino 
-          del ojo se vuelve opaco o nublado. 
-          Esta opacidad dificulta el paso de la luz y provoca una visión borrosa, descolorida o con destellos. 
-          </p>
-          <p>
-          Las cataratas suelen desarrollarse de forma gradual con la edad, 
-          aunque también pueden aparecer por causas genéticas, traumatismos, 
-          enfermedades o el uso prolongado de ciertos medicamentos. 
-          </p>
-          <p>
-          Sus principales efectos son:
-          </p>
-          <ul>
-            <li>Disminución de la agudeza visual</li>
-            <li>Aumento de la sensibilidad a la luz</li>
-            <li>Fatiga visual y dolores de cabeza</li>
-          </ul>
-        </div>
-
         <div className="diagram-section">
           <div className="canvas-wrapper">
             <Canvas camera={{ position: [0, 1, 3]}} shadows>
@@ -46,6 +24,27 @@ export default function definition() {
               <OrbitControls />
             </Canvas>
           </div>
+        </div>
+
+        <div className="info-section">
+          <h2 className="mb-3" style={{ color: 'var(--color-2)' }}>¿Qué son?</h2>
+          <p>
+          Las cataratas oculares son una condición visual común que ocurre cuando el cristalino 
+          del ojo se vuelve opaco o nublado. 
+          Dificultando el paso de la luz y provocando una visión borrosa, descolorida o con destellos. 
+          </p>
+          <p>
+          Las cataratas suelen desarrollarse de forma gradual con la edad, por causas genéticas, traumatismos, 
+          enfermedades o el uso prolongado de ciertos medicamentos. 
+          </p>
+          <p>
+          Sus principales efectos son:
+          </p>
+          <ul>
+            <li>Disminución de la agudeza visual</li>
+            <li>Aumento de la sensibilidad a la luz</li>
+            <li>Fatiga visual y dolores de cabeza</li>
+          </ul>
         </div>
       </div>
   );
